0eea0c21993f56108b38dc59b4bf91f59e937d6d
nagamani <nagamani.onteru@gmail.com>
2018-07-20 10:11:57 +0000

    src is commit

---
+++ myjavaapp/src/main/java/com/dl/proj3/App.java
@@ -0,0 +1,13 @@
+package com.dl.proj3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
