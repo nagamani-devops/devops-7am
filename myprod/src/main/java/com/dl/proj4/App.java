526d8ffe0b4dccfab201a176eca328c3eeebbb7a
nagamani <nagamani.onteru@gmail.com>
2018-07-20 10:39:22 +0000

    prod is commited

---
+++ myprod/src/main/java/com/dl/proj4/App.java
@@ -0,0 +1,13 @@
+package com.dl.proj4;
+
+/**
+ * Hello product!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello product!" );
+    }
+}
